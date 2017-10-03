cd2800f34d02a2ab31631dfc170836d045466f3c
Wangohan <31858283+Wangohan@users.noreply.github.com>
2017-10-03 14:24:29 +0300

    Revert "Revert "Update header.php""
    This reverts commit 155fe1f4f0dc643867c14f08bcfe7a16117f0680.

--- header.php
+++ header.php
@@ -1,23 +1,22 @@
 <html lang="en">
 <?php wp_head(); ?>
 <header class="bg_1">
-            <div class="container">
-                <div class="row logo">
-                    <div class="col-md-6 col-sm-6 text-left">
-                        
-						<b>Шапка</b>
-                    </div>
-                </div>
-                <div class="row">
-                    <h1>Плагин регистрации</h1>
-                </div>
-                <div class="row slogan">
-                    <p class="col regions">Регистрация пользователей.</span>
-                </div>
-                <div class="row ancor">
-                    <a href="#bg_10">
-                        <div class="button_large bth_puls">Регистрация</div>
-                    </a>
-                </div>
+    <div class="container">
+        <div class="row logo">
+            <div class="col-md-6 col-sm-6 text-left">
+                <b>Шапка</b>
             </div>
-        </header>
+        </div>
+        <div class="row">
+            <h1>Плагин регистрации</h1>
+        </div>
+        <div class="row slogan">
+            <p class="col regions">Регистрация пользователей.</p>
+        </div>
+        <div class="row ancor">
+            <a href="#bg_10">
+                <div class="button_large bth_puls">Регистрация</div>
+            </a>
+        </div>
+    </div>
+</header>
